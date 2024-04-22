966b5673b904c194cfaa8b8cfeaad69315c34345
Muhindo-Galien <muhindogalien512@gmail.com>
2024-04-22 04:05:30 +0300

    feat:NFT Minter

---
+++ contracts/NFTMinter.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.20;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract NFTMinter is Initializable, ERC721Upgradeable, ERC721URIStorageUpgradeable {
+    address public admin;
+    uint256 private _tokenIds;
+
+    struct mintedNft {
+        uint256 nftId;
+        uint256 token_id;
+    }
+
+    mapping(address => mintedNft[]) internal mints;
+    mapping(address => bool) public eligibleAddresses;
+
+    modifier onlyEligible() {
+        require(eligibleAddresses[msg.sender], "Address is not eligible for minting");
+        _;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only admin can call this function");
+        _;
+    }
+
+    function initialize(string memory _name, string memory _symbol) public initializer {
+        __ERC721_init(_name, _symbol);
+        admin = msg.sender;
+    }
+
+    function addEligibleAddress(address _address) external{
+        eligibleAddresses[_address] = true;
+    }
+
+    function mintsByUser(address user) public view returns (mintedNft[] memory) {
+        return mints[user];
+    }
+
+    function safeMint(address to, uint256 nftId, string memory uri) public onlyEligible {
+        require(to == msg.sender, "You can't mint this NFT");
+
+        _tokenIds++;
+
+        _mint(to, _tokenIds);
+
+        _setTokenURI(_tokenIds, uri);
+
+        mints[to].push(mintedNft(nftId, _tokenIds));
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721Upgradeable, ERC721URIStorageUpgradeable) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function tokenURI(uint256 tokenId) public view override(ERC721Upgradeable, ERC721URIStorageUpgradeable) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+}
---
+++ ts-src/deploy/deploy-nftMinter.ts
@@ -0,0 +1,20 @@
+import { ethers} from "./index";
+
+async function main() {
+
+  // Deploy NFTMinter contract
+  console.log("Deploying NFTMinter contract");
+  const NFTMinter = await ethers.getContractFactory("NFTMinter");
+  const nftMinter = await NFTMinter.deploy()
+  await nftMinter.waitForDeployment();
+  console.log(" NFTMinter contract deployed at", nftMinter.target);
+
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+      console.error(error);
+      process.exit(1);
+  });
+  // NFTMinter contract deployed at 0x5DBcfb13aC3aab5cCFD7d2c9b49b9017b7477EC8
